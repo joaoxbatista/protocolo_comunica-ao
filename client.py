7d101abcab834411fdae86d1c092bd52a49b98c8
João Batista Gomes Silva <jhonxbatista@gmail.com>
2018-09-30 20:21:06 -0300

    versão 1 - rsa e fernet

--- client.py
+++ client.py
@@ -3,82 +3,157 @@ import socket
 import json
 import base64
 import os
-from classes.Client import Client
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
 
-cm = Client(['fernet'], ['rsa'])
+'''
+Verifica se a mensagem recebida é um JSON
+'''
+def is_json(myjson):
+	try:
+		json_object = json.loads(myjson)
+	except ValueError as e:
+		return False
+	return True
 
-local_address = ('localhost', 5001)
-dest_address = ('localhost' , 8000)
-dest_address = ('localhost' , 5000)
+def formatMessage(type ,key, data):
+	result =  {
+		"type_message": type,
+		"data": {
+			key: data
+		}
+	}
+	result = json.dumps(result)
+	return result.encode("utf-8")
+
+def generate_keys():
+	
+	# Gera chave simetrica
+	if(selected["simetric"] == "fernet"):
+		keys["simetric"] = Fernet.generate_key()
+
+	# Seleção do algoritmo assimétrico
+	if(selected["assimetric"] == 'rsa'):
+		# Gera uma chave privada
+		keys["private_rsa"] = rsa.generate_private_key(
+			public_exponent=65537,
+			key_size=1028,
+			backend=default_backend()
+		)
+
+		keys["public_rsa"] = keys["private_rsa"].public_key()
+	
+
+def encrypt_simetric_key():
+    simetric_key = server_keys["public_key"].encrypt(
+         keys["simetric"],
+         padding.OAEP(
+             mgf=padding.MGF1(algorithm=hashes.SHA256()),
+             algorithm=hashes.SHA256(),
+             label=None
+         )
+    )
+
+    return simetric_key.decode("latin-1")
+
+def encrypt_message(message):
+	f = Fernet(keys["simetric"])
+	encrypt_message = f.encrypt(message)
+	return encrypt_message.decode('utf-8')
+
+'''
+Padrão de Comunicação Selecionado
+'''
+suport = {
+    "simetric": ['fernet'],
+    "assimetric": ['rsa']
+}
+
+selected = {
+    "simetric": None,
+    "assimetric": None,
+}
+
+keys = {
+    "simetric": None,
+    "private_rsa": None,
+    "public_rsa": None
+}
+
+server_keys = {
+	"public_key": None
+}
+
+
+# '''
+# 	1 - Conexão com o servidor
+# '''
+local_address = ('localhost', 5000)
+dest_address = ('localhost' , 8001)
+
+
+# '''
+# 	2 - Enviar a primeira requisição para o acordo de comunicação
+# '''
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(dest_address)
+server.send(formatMessage("accord_comunication", "suport", suport))
+
+# '''
+#   3 - Escuta reposta do tipo de comunicação selecionada 
+# '''
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-# 1 - Enviar a primeira requisição para o acordo de comunicação
-server.connect(dest_address)
-server.send(cm.comunication.getSuport(True))
-server.close()
-
-# 2 - Escuta repostas do servidor 
-
 client.bind(local_address)
 client.listen(1)
 conn_server, add_server = client.accept()
 
 while True:
 	message = conn_server.recv(1024).decode('utf-8')
-	
-	if(message != b''):
-		try:
-			if(json.loads(message)):
-				response = json.loads(message)
-				cm.comunication.keys['public_key'] = base64.decodestring(response["key"].encode('utf-8'))
-				cm.comunication.selected = response["selected"]
-				
 
-		except Exception as e:
-			print(e)
+	if(is_json(message)):
+		data = json.loads(message)
 
-		# print("Mensagem recebida: " + message)
-		break
-conn_server.close		
+		if("type_message" in data.keys()):
+			if(data["type_message"] == "selected_suport"):
+				selected = data["data"]["selected"]
+				server_keys["public_key"] = load_pem_public_key(selected["public_key"].encode('utf-8'), backend=default_backend())
+				generate_keys()	
+				server.send(formatMessage("send_simetric_key", "simetric_key", encrypt_simetric_key()))
+				# print(formatMessage("send_simetric_key", "simetric_key", encrypt_simetric_key()))
+				# server.send(formatMessage("send_simetric_key", "simetric_key", keys["simetric"].decode('utf-8')))
+				break		
+			
+conn_server.close()		
 
 
-# 3 - Envia a chave simetrica encriptada para o servidor
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect(dest_address)
-print("Enviando chave simetrica")
-print(cm.getSimetricKey())
-server.send(cm.getSimetricKey())
-server.close()
+'''
+	4 - Envia mensagens para o servidor
+'''
+
+print("request for www.server.com ... ")
+print("-------------------------------")
+print("insert exit to exit program.")
+print("-------------------------------\n")
+
 
 while True:
-	input()
+    try:
+        message_to_server = input("insert your text: \n")
 
-# print("request for www.server.com ... ")
-# print("-------------------------------")
-# print("insert exit to exit program.")
-# print("-------------------------------\n")
+        if(message_to_server == 'exit'):
+        	break
+        else:
+        	server.send(formatMessage("message", "message", encrypt_message(message_to_server.encode('utf-8'))))
+        	print("* your message is sended!\n")
 
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.connect(dest_address)
 
-# while True:
-#     try:
-#         message_to_server = input("insert your text: \n")
+    except Exception as e:
+      print(e)
+      break
 
-#         if(message_to_server == 'exit'):
-#         	break
-
-#         if(server.send(message_to_server.encode('utf-8'))):
-#         	print("* your message is sended!\n")
-
-        
-
-#     except Exception as e:
-#       print(e)
-#       break
-
-# server.close()
+server.close()
--- server.py
+++ server.py
@@ -3,8 +3,18 @@ import socket
 import json
 import os
 import sys
-from classes.Server import Server
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding 
+from cryptography.hazmat.primitives import hashes
 
+'''
+Métodos utilitários
+'''
+# 1 - Verifica se a string pode ser convertida para json
 def is_json(myjson):
   try:
     json_object = json.loads(myjson)
@@ -12,55 +22,150 @@ def is_json(myjson):
     return False
   return True
 
-cm = Server(['fernet'], ['rsa'])
-	
+# 1 - Formata mensagens para envio
+def formatMessage(type_message ,key, data):
+	result =  {
+		"type_message": type_message,
+		"data": {
+			key: data
+		}
+    }
+	result = json.dumps(result)
+	return result.encode("utf-8")
 
-local_address = ('localhost', 8000)
-local_address = ('localhost', 5000)
-dest_address = ('localhost' , 5001)
+# 2 - Seleciona os tipos em comum de comunicação
+def select_comunication(server_suport, client_suport, selected):
+	for server_simetric in server_suport["simetric"]:
+	    for client_simetric in client_suport["simetric"]:
+	    	if(server_simetric == client_simetric):
+	        	selected["simetric"] = server_simetric
 
+
+	for server_assimetric in server_suport["assimetric"]:
+	    for client_assimetric in client_suport["assimetric"]:
+	        if(server_assimetric == client_assimetric):
+	            selected["assimetric"] = server_assimetric
+
+# 3 - Gerar chaves
+def generate_keys(selected, keys):
+
+	# Seleção do algoritmo assimétrico
+	if(selected["assimetric"] == 'rsa'):
+
+		# Gera uma chave privada
+		keys["private_rsa"] = rsa.generate_private_key(
+			public_exponent=65537,
+			key_size=1028,
+			backend=default_backend()
+		)
+
+		keys["public_rsa"] = keys["private_rsa"].public_key()
+
+# 4 - Obter chave publica descerializada
+def get_public_key():
+	return keys["public_rsa"].public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    ).decode('utf-8')
+
+# 5 - Desencripta mesnsagem com chave simetrica do cliente
+def decrypt_message(message):
+	f = Fernet(keys["simetric"])
+	return f.decrypt(message.encode('utf-8')).decode('utf-8')
+
+
+'''
+Padrão de Comunicação Selecionado
+'''
+suport = {
+    "simetric": ['fernet'],
+    "assimetric": ['rsa']
+}
+
+selected = {
+    "simetric": None,
+    "assimetric": None,
+    "public_key": None
+}
+
+keys = {
+    "simetric": "",
+    "private_rsa": None,
+    "public_rsa": None
+}
+
+'''
+Definir a chave Privada
+'''
+
+local_address = ('localhost', 8001)
+dest_address = ('localhost' , 5000)
+
+'''
+Espera conexão com o cliente
+'''
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#Escuta respotas do cliente
 server.bind(local_address)
 server.listen(1)
 
 print("wwww.server.com -> waiting for client comunication \n")
 
 while True:
-	
+
 	client, client_address = server.accept()
 	pid = os.fork()
 
 	if(pid == 0):
-	
 		server.close()
 		print(str(client_address) + "connected")
 
 		while True:
 			message = client.recv(1024).decode('utf-8')
 
+			if(not message):
+				print("not message erro")
+				
 			# Caso a mensagem enviada seja um JSON
-			if(is_json(message)):
+			elif(is_json(message)):
 				data = json.loads(message)
 
-				if("assimetric" in data.keys()):
-					cm.accord(message, True)
-					print(cm.private_key)
-					print("make accord with client pattern successfuly!")
-					server_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-					server_sender.connect(dest_address)
-					server_sender.send(cm.getResponseAccord())
-					server_sender.close()
-					
-				if("simetric_key" in data.keys()):
-					print("simetric key for comunication: ")
-					print(cm.private_key)
+				if("type_message" in data.keys()):
 
+					if(data["type_message"] == "accord_comunication"):
+						select_comunication(suport, data["data"]["suport"], selected)
+						generate_keys(selected, keys)
+						selected["public_key"] = get_public_key()
 
-			elif(message and not is_json(message)):
-				print("new message: " + str(message))
+						# Enviar a chave publica para o cliente 
+						server_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+						server_sender.connect(dest_address)
+						server_sender.send(formatMessage("selected_suport", "selected", selected))
+						server_sender.close()
+						
 
+					elif(data["type_message"] == "send_simetric_key"):
+						keys["simetric"] = data['data']['simetric_key']
+						latin1 = keys['simetric'].encode('latin-1')
+						keys["simetric"] = keys['private_rsa'].decrypt(
+						     latin1,
+						     padding.OAEP(
+						         mgf=padding.MGF1(algorithm=hashes.SHA256()),
+						         algorithm=hashes.SHA256(),
+						         label=None
+						     )
+						)
+
+					elif(data["type_message"] == "message"):
+						print("-----------------------------------------------------------")
+						print("\n* message encrypted: " + data["data"]["message"])
+						print("\n* message decrypted: " + decrypt_message(data["data"]["message"]))
+						print("-----------------------------------------------------------")
+						
+				else:
+					print("server not suport comunication with client")
+			else:
+				pass
+				
 		print("wwww.server.com -> close connection with " + str(client_address) +" \n ")
 	else:
 		client.close()
